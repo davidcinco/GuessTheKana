ab97f31316baaaca59988c542512b1bdaa0b6136
David Cinco <cincoisc@gmail.com>
2024-04-14 10:23:58 -0700

    fePages commit

--- FE/js/index.js
+++ FE/js/index.js
@@ -1,7 +1,9 @@
-const API = require('../js/api');
+const API = require('../js/api.js');
 
 async function runApi(){
     const response = await fetch(API);
     const res = await response.json();
     console.log(res);
 }
+
+runApi();
