08746b0f6a1bdabade81327069657b15cff2bdf2
David Cinco <cincoisc@gmail.com>
2024-04-18 17:57:49 -0700

    UI Changes; hiragana and katakana charts

---
+++ FE/js/hiragana/hiragana.js
@@ -0,0 +1,12 @@
+form = document.getElementById("form-bg");
+
+
+function openForm(){
+    form.style.display = "flex";
+    form.style.justifyContent = "center";
+    form.style.alignItems = "center";
+}
+
+function closeForm() {
+    form.style.display = "none";
+}
